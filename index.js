7b7914991b08abf674591551e52dd5ae9052447c
Georgy <groxmirk@gmail.com>
2023-07-30 07:04:04 +0400

    Done. Working as intended.

--- index.js
+++ index.js
@@ -1,37 +1,78 @@
-// your_script.js
+
+const SPREADSHEET_ID = '1v9qxDmXpAZehEUQoOeEy8pBmZUSHJJxZ5mb4f87O750';
+
+// Function to get rows from a Google Sheet
+async function getRowsFromGoogleSheet(sheetId) {
+  try {
+    const response = await fetch(`https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:json`);
+    const text = await response.text();
+    const jsonString = /({.*})/.exec(text)[1];
+    const data = JSON.parse(jsonString);
+
+    // Extract the rows from the response (data.table.rows)
+    if (data && data.table && data.table.rows) {
+      const rows = data.table.rows;
+
+      const mappedData = rows.map((row) => {
+        const [lat, lng, title, linkTitle, url, description] = row.c.map((cell) => cell.v);
+        return { 
+            lat: parseFloat(lat), 
+            lng: parseFloat(lng), 
+            title: title.trim(),
+            linkTitle: linkTitle.trim(),
+            url: url.trim(),
+            description: description.trim()
+        };
+      });
+      console.log(mappedData);
+      
+      return mappedData;
+    } else {
+      console.error('Error: Invalid response from Google Sheets API');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error retrieving data:', error.message);
+    return null;
+  }
+}
+
+// Call the function with the sample spreadsheet ID
+const spreadsheetId = SPREADSHEET_ID; // Replace this with your actual spreadsheet ID
+getRowsFromGoogleSheet(spreadsheetId)
+  .then((rows) => {
+    if (rows) {
+      initMap(rows)
+      console.log(rows);
+    }
+  })
+  .catch((error) => {
+    console.error('Error:', error.message);
+  });
 
 // Function to initialize the map
-function initMap() {
+function initMap(locations) {
     // Set the initial coordinates and zoom level of the map
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([40.177628, 44.512546], 10);
 
     // Add the OpenStreetMap tile layer to the map
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?lang=en', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Function to fetch data from the Google Sheet (Mock implementation)
-    function fetchDataFromGoogleSheet() {
-        // In a real-world scenario, you would use the Google Sheets API to fetch data
-        // For simplicity, we're using a mock dataset here
-        return [
-            { lat: 51.5, lng: -0.09, popup: "Location 1" },
-            { lat: 51.51, lng: -0.1, popup: "Location 2" },
-            { lat: 51.52, lng: -0.11, popup: "Location 3" },
-            // Add more locations as needed
-        ];
-    }
-
     // Fetch data from the Google Sheet (Mock implementation)
-    var locations = fetchDataFromGoogleSheet();
-
-    // Loop through the locations array and add markers to the map
-    for (var i = 0; i < locations.length; i++) {
-        var location = locations[i];
+    // const locations = getRowsFromGoogleSheet(SPREADSHEET_ID);
+    console.log(locations)
+    locations.forEach((location) => {
         var marker = L.marker([location.lat, location.lng]).addTo(map);
-        marker.bindPopup("<b>" + location.popup + "</b>").openPopup();
-    }
+        marker.bindPopup(
+            `<b>${location.title}</b><br>` +
+            `<a href="${location.url}">${location.linkTitle}</a><br>` +
+            location.description 
+          ).openPopup();
+      });
 }
 
 // Call the initMap function when the page loads
-window.onload = initMap;
+// window.onload = initMap;
